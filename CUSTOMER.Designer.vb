daf0fab5bf3a4c3a9e76bbc6a32e7f0c775fa7ab
Christian John Ibañez <109792510+siji1007@users.noreply.github.com>
2024-04-16 17:35:13 +0800

    Updated code

--- CUSTOMER.Designer.vb
+++ CUSTOMER.Designer.vb
@@ -22,9 +22,29 @@ Partial Class CUSTOMER
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle1 As DataGridViewCellStyle = New DataGridViewCellStyle()
+        Dim DataGridViewCellStyle2 As DataGridViewCellStyle = New DataGridViewCellStyle()
+        Dim DataGridViewCellStyle3 As DataGridViewCellStyle = New DataGridViewCellStyle()
         Lbl_customer = New Label()
-        DataGridView1 = New DataGridView()
-        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        customer_datagridview = New DataGridView()
+        cl_ID = New DataGridViewTextBoxColumn()
+        CL_NAME = New DataGridViewTextBoxColumn()
+        CL_ADDRESS = New DataGridViewTextBoxColumn()
+        CL_EMAIL = New DataGridViewTextBoxColumn()
+        CL_CNUMBER = New DataGridViewTextBoxColumn()
+        txt_cname = New TextBox()
+        txt_caddress = New TextBox()
+        txt_cemail = New TextBox()
+        txt_cnumber = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Add_cust_btn = New Button()
+        Update_cust_btn = New Button()
+        Delete_cust_btn = New Button()
+        Clear_cust_btn = New Button()
+        CType(customer_datagridview, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
         ' 
         ' Lbl_customer
@@ -39,15 +59,196 @@ Partial Class CUSTOMER
         Lbl_customer.TabIndex = 0
         Lbl_customer.Text = "CUSTOMER"
         ' 
-        ' DataGridView1
+        ' customer_datagridview
         ' 
-        DataGridView1.Anchor = AnchorStyles.Left Or AnchorStyles.Right
-        DataGridView1.BackgroundColor = Color.FromArgb(CByte(30), CByte(39), CByte(46))
-        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        DataGridView1.Location = New Point(12, 86)
-        DataGridView1.Name = "DataGridView1"
-        DataGridView1.Size = New Size(1109, 301)
-        DataGridView1.TabIndex = 1
+        customer_datagridview.Anchor = AnchorStyles.Left Or AnchorStyles.Right
+        customer_datagridview.BackgroundColor = Color.FromArgb(CByte(30), CByte(39), CByte(46))
+        DataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle1.BackColor = Color.FromArgb(CByte(30), CByte(39), CByte(46))
+        DataGridViewCellStyle1.Font = New Font("Microsoft Sans Serif", 14.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        DataGridViewCellStyle1.ForeColor = Color.White
+        DataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight
+        DataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText
+        DataGridViewCellStyle1.WrapMode = DataGridViewTriState.True
+        customer_datagridview.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle1
+        customer_datagridview.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        customer_datagridview.Columns.AddRange(New DataGridViewColumn() {cl_ID, CL_NAME, CL_ADDRESS, CL_EMAIL, CL_CNUMBER})
+        DataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle2.BackColor = Color.FromArgb(CByte(30), CByte(39), CByte(46))
+        DataGridViewCellStyle2.Font = New Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        DataGridViewCellStyle2.ForeColor = Color.White
+        DataGridViewCellStyle2.SelectionBackColor = SystemColors.Highlight
+        DataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText
+        DataGridViewCellStyle2.WrapMode = DataGridViewTriState.False
+        customer_datagridview.DefaultCellStyle = DataGridViewCellStyle2
+        customer_datagridview.GridColor = SystemColors.Window
+        customer_datagridview.Location = New Point(12, 86)
+        customer_datagridview.Name = "customer_datagridview"
+        customer_datagridview.ReadOnly = True
+        DataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle3.BackColor = Color.FromArgb(CByte(30), CByte(39), CByte(46))
+        DataGridViewCellStyle3.Font = New Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        DataGridViewCellStyle3.ForeColor = SystemColors.WindowText
+        DataGridViewCellStyle3.SelectionBackColor = SystemColors.Highlight
+        DataGridViewCellStyle3.SelectionForeColor = SystemColors.HighlightText
+        DataGridViewCellStyle3.WrapMode = DataGridViewTriState.True
+        customer_datagridview.RowHeadersDefaultCellStyle = DataGridViewCellStyle3
+        customer_datagridview.RowHeadersVisible = False
+        customer_datagridview.ScrollBars = ScrollBars.Vertical
+        customer_datagridview.Size = New Size(1109, 301)
+        customer_datagridview.TabIndex = 1
+        ' 
+        ' cl_ID
+        ' 
+        cl_ID.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells
+        cl_ID.HeaderText = "ID"
+        cl_ID.Name = "cl_ID"
+        cl_ID.ReadOnly = True
+        cl_ID.Width = 52
+        ' 
+        ' CL_NAME
+        ' 
+        CL_NAME.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill
+        CL_NAME.HeaderText = "NAME"
+        CL_NAME.Name = "CL_NAME"
+        CL_NAME.ReadOnly = True
+        ' 
+        ' CL_ADDRESS
+        ' 
+        CL_ADDRESS.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill
+        CL_ADDRESS.HeaderText = "ADDRESS"
+        CL_ADDRESS.Name = "CL_ADDRESS"
+        CL_ADDRESS.ReadOnly = True
+        ' 
+        ' CL_EMAIL
+        ' 
+        CL_EMAIL.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill
+        CL_EMAIL.HeaderText = "EMAIL"
+        CL_EMAIL.Name = "CL_EMAIL"
+        CL_EMAIL.ReadOnly = True
+        ' 
+        ' CL_CNUMBER
+        ' 
+        CL_CNUMBER.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill
+        CL_CNUMBER.HeaderText = "CONTACT NUMBER"
+        CL_CNUMBER.Name = "CL_CNUMBER"
+        CL_CNUMBER.ReadOnly = True
+        ' 
+        ' txt_cname
+        ' 
+        txt_cname.Location = New Point(143, 450)
+        txt_cname.Name = "txt_cname"
+        txt_cname.Size = New Size(230, 23)
+        txt_cname.TabIndex = 2
+        ' 
+        ' txt_caddress
+        ' 
+        txt_caddress.Location = New Point(143, 492)
+        txt_caddress.Name = "txt_caddress"
+        txt_caddress.Size = New Size(230, 23)
+        txt_caddress.TabIndex = 3
+        ' 
+        ' txt_cemail
+        ' 
+        txt_cemail.Location = New Point(143, 534)
+        txt_cemail.Name = "txt_cemail"
+        txt_cemail.Size = New Size(230, 23)
+        txt_cemail.TabIndex = 4
+        ' 
+        ' txt_cnumber
+        ' 
+        txt_cnumber.Location = New Point(731, 450)
+        txt_cnumber.Name = "txt_cnumber"
+        txt_cnumber.Size = New Size(230, 23)
+        txt_cnumber.TabIndex = 5
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.BackColor = Color.Transparent
+        Label1.Font = New Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label1.ForeColor = Color.White
+        Label1.Location = New Point(17, 453)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(55, 20)
+        Label1.TabIndex = 6
+        Label1.Text = "NAME"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.BackColor = Color.Transparent
+        Label2.Font = New Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label2.ForeColor = Color.White
+        Label2.Location = New Point(17, 495)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(89, 20)
+        Label2.TabIndex = 7
+        Label2.Text = "ADDRESS"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.BackColor = Color.Transparent
+        Label3.Font = New Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label3.ForeColor = Color.White
+        Label3.Location = New Point(17, 537)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(58, 20)
+        Label3.TabIndex = 8
+        Label3.Text = "EMAIL"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.BackColor = Color.Transparent
+        Label4.Font = New Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label4.ForeColor = Color.Black
+        Label4.Location = New Point(646, 453)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(79, 20)
+        Label4.TabIndex = 9
+        Label4.Text = "NUMBER"
+        ' 
+        ' Add_cust_btn
+        ' 
+        Add_cust_btn.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left
+        Add_cust_btn.Location = New Point(27, 814)
+        Add_cust_btn.Name = "Add_cust_btn"
+        Add_cust_btn.Size = New Size(111, 53)
+        Add_cust_btn.TabIndex = 10
+        Add_cust_btn.Text = "CREATE"
+        Add_cust_btn.UseVisualStyleBackColor = True
+        ' 
+        ' Update_cust_btn
+        ' 
+        Update_cust_btn.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left
+        Update_cust_btn.Location = New Point(266, 814)
+        Update_cust_btn.Name = "Update_cust_btn"
+        Update_cust_btn.Size = New Size(111, 53)
+        Update_cust_btn.TabIndex = 11
+        Update_cust_btn.Text = "UPDATE"
+        Update_cust_btn.UseVisualStyleBackColor = True
+        ' 
+        ' Delete_cust_btn
+        ' 
+        Delete_cust_btn.Anchor = AnchorStyles.Bottom Or AnchorStyles.Right
+        Delete_cust_btn.Location = New Point(789, 814)
+        Delete_cust_btn.Name = "Delete_cust_btn"
+        Delete_cust_btn.Size = New Size(111, 53)
+        Delete_cust_btn.TabIndex = 12
+        Delete_cust_btn.Text = "DELETE"
+        Delete_cust_btn.UseVisualStyleBackColor = True
+        ' 
+        ' Clear_cust_btn
+        ' 
+        Clear_cust_btn.Anchor = AnchorStyles.Bottom Or AnchorStyles.Right
+        Clear_cust_btn.Location = New Point(1012, 814)
+        Clear_cust_btn.Name = "Clear_cust_btn"
+        Clear_cust_btn.Size = New Size(111, 53)
+        Clear_cust_btn.TabIndex = 13
+        Clear_cust_btn.Text = "CLEAR"
+        Clear_cust_btn.UseVisualStyleBackColor = True
         ' 
         ' CUSTOMER
         ' 
@@ -57,14 +258,43 @@ Partial Class CUSTOMER
         BackgroundImageLayout = ImageLayout.Center
         ClientSize = New Size(1133, 890)
         ControlBox = False
-        Controls.Add(DataGridView1)
+        Controls.Add(Clear_cust_btn)
+        Controls.Add(Delete_cust_btn)
+        Controls.Add(Update_cust_btn)
+        Controls.Add(Add_cust_btn)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(txt_cnumber)
+        Controls.Add(txt_cemail)
+        Controls.Add(txt_caddress)
+        Controls.Add(txt_cname)
+        Controls.Add(customer_datagridview)
         Controls.Add(Lbl_customer)
         Name = "CUSTOMER"
-        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        CType(customer_datagridview, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
         PerformLayout()
     End Sub
 
     Friend WithEvents Lbl_customer As Label
-    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents customer_datagridview As DataGridView
+    Friend WithEvents txt_cname As TextBox
+    Friend WithEvents txt_caddress As TextBox
+    Friend WithEvents txt_cemail As TextBox
+    Friend WithEvents txt_cnumber As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Add_cust_btn As Button
+    Friend WithEvents Update_cust_btn As Button
+    Friend WithEvents Delete_cust_btn As Button
+    Friend WithEvents Clear_cust_btn As Button
+    Friend WithEvents cl_ID As DataGridViewTextBoxColumn
+    Friend WithEvents CL_NAME As DataGridViewTextBoxColumn
+    Friend WithEvents CL_ADDRESS As DataGridViewTextBoxColumn
+    Friend WithEvents CL_EMAIL As DataGridViewTextBoxColumn
+    Friend WithEvents CL_CNUMBER As DataGridViewTextBoxColumn
 End Class
--- CUSTOMER.vb
+++ CUSTOMER.vb
@@ -1,4 +1,6 @@
-﻿Public Class CUSTOMER
+﻿Imports MySql.Data.MySqlClient
+
+Public Class CUSTOMER
     Private Sub Lbl_customer_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Lbl_customer.Anchor = AnchorStyles.None
         Lbl_customer.TextAlign = ContentAlignment.MiddleCenter
@@ -9,5 +11,248 @@
         Dim centerX As Integer = (Me.ClientSize.Width - Lbl_customer.Width) \ 2
         Lbl_customer.Location = New Point(centerX, Lbl_customer.Location.Y)
 
+
+        LoadDataCustomer()
+
+
     End Sub
+
+
+    'LOAD HERE THE DATA FROM DATABASE TO DATAGRIDVIEW 
+    Private Sub LoadDataCustomer()
+        Try
+            If openDB() Then
+                Dim query As New MySqlCommand("SELECT * FROM customer", Conn)
+                Using dr As MySqlDataReader = query.ExecuteReader
+                    While dr.Read
+                        Dim rowIndex As Integer = customer_datagridview.Rows.Add(dr.Item("Cust_ID"), dr.Item("Cust_name"), dr.Item("Cust_address"), dr.Item("Cust_email"), dr.Item("Cust_cnumber"))
+
+                    End While
+                End Using
+            Else
+                MessageBox.Show("Failed to coonect to database")
+
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+
+        End Try
+    End Sub
+
+    Private Sub customer_datagridview_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles customer_datagridview.CellContentClick
+        If e.RowIndex >= 0 AndAlso e.ColumnIndex >= 0 Then
+            Dim selectedRow As DataGridViewRow = customer_datagridview.Rows(e.RowIndex)
+
+            'Access the data in the selected row as needed 
+            Dim CustID As String = selectedRow.Cells("cl_ID").Value.ToString()
+            Dim CustName As String = selectedRow.Cells("CL_NAME").Value.ToString()
+            Dim CustAddress As String = selectedRow.Cells("CL_ADDRESS").Value.ToString()
+            Dim CustEmail As String = selectedRow.Cells("CL_EMAIL").Value.ToString()
+            Dim CustCnumber As String = selectedRow.Cells("CL_CNUMBER").Value.ToString()
+
+
+
+            For Each row As DataGridViewRow In customer_datagridview.Rows
+                If row.Index = e.RowIndex Then
+                    row.Selected = True
+                Else
+                    row.Selected = False
+                End If
+            Next
+
+
+            'Display the selected row on its corresponding txtbox 
+
+            txt_cname.Text = CustName
+            txt_caddress.Text = CustAddress
+            txt_cemail.Text = CustEmail
+            txt_cnumber.Text = CustCnumber
+
+        End If
+    End Sub
+
+
+
+    'add function
+    Private Sub Add_cust_btn_Click(sender As Object, e As EventArgs) Handles Add_cust_btn.Click
+        Dim Cust_name As String = txt_cname.Text.Trim()
+        Dim Cust_address As String = txt_caddress.Text.Trim()
+        Dim Cust_email As String = txt_cemail.Text.Trim()
+        Dim Cust_cnumber As Integer
+
+
+        If String.IsNullOrWhiteSpace(Cust_name) OrElse String.IsNullOrWhiteSpace(Cust_address) OrElse String.IsNullOrWhiteSpace(Cust_email) OrElse Not Integer.TryParse(txt_cnumber.Text, Cust_cnumber) Then
+            MessageBox.Show("Please fill the information properly")
+
+
+        Else
+            If openDB() Then
+                Dim query As String = "INSERT INTO customer VALUES(NULL, @C_name, @C_address, @C_email, @C_cnumber)"
+                Dim cmd As New MySqlCommand(query, Conn)
+                cmd.Parameters.AddWithValue("@C_name", Cust_name.ToUpper())
+                cmd.Parameters.AddWithValue("@C_address", Cust_address.ToUpper())
+                cmd.Parameters.AddWithValue("@C_email", Cust_email.ToUpper())
+                cmd.Parameters.AddWithValue("@C_cnumber", Cust_cnumber)
+
+
+                Try
+                    cmd.ExecuteNonQuery()
+                    MessageBox.Show("Customer is added Successfully!")
+                    customer_datagridview.Rows.Clear()
+                    LoadDataCustomer()
+
+
+                    txt_cname.Clear()
+                    txt_caddress.Clear()
+                    txt_cemail.Clear()
+                    txt_cnumber.Clear()
+
+                Catch ex As Exception
+                    MessageBox.Show(ex.Message)
+
+                Finally
+                    closeDB()
+
+                End Try
+            Else
+                MessageBox.Show("The database is not connected!")
+
+            End If
+
+        End If
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    End Sub
+
+    Private Sub Update_cust_btn_Click(sender As Object, e As EventArgs) Handles Update_cust_btn.Click
+        Dim Cust_id As Integer
+        If Integer.TryParse(customer_datagridview.CurrentRow.Cells("cl_ID").Value.ToString(), Cust_id) Then
+
+            Dim Cust_name As String = txt_cname.Text
+            Dim Cust_address As String = txt_caddress.Text
+            Dim Cust_email As String = txt_cemail.Text
+            Dim Cust_cnumber As Integer
+
+            If String.IsNullOrWhiteSpace(Cust_name) OrElse String.IsNullOrWhiteSpace(Cust_address) OrElse String.IsNullOrWhiteSpace(Cust_email) OrElse Not Integer.TryParse(txt_cnumber.Text, Cust_cnumber) Then
+                MessageBox.Show("Please fill the information properly")
+
+            Else
+                If openDB() Then
+                    Dim query As String = "UPDATE customer SET  Cust_name = @Cname, Cust_address = @C_address, Cust_email = @C_email, Cust_cnumber = @C_cnumber WHERE Cust_ID = @C_ID"
+                    Dim cmd As New MySqlCommand(query, Conn)
+                    cmd.Parameters.AddWithValue("@C_ID", Cust_id)
+                    cmd.Parameters.AddWithValue("@Cname", Cust_name.ToUpper())
+                    cmd.Parameters.AddWithValue("@C_address", Cust_address.ToUpper())
+                    cmd.Parameters.AddWithValue("@C_email", Cust_email.ToUpper())
+                    cmd.Parameters.AddWithValue("@C_cnumber", Cust_cnumber)
+
+                    Try
+                        cmd.ExecuteNonQuery()
+                        customer_datagridview.Rows.Clear()
+                        LoadDataCustomer()
+
+                        txt_cname.Clear()
+                        txt_caddress.Clear()
+                        txt_cemail.Clear()
+                        txt_cnumber.Clear()
+
+                    Catch ex As Exception
+                        MessageBox.Show(ex.Message)
+
+                    Finally
+                        closeDB()
+
+                    End Try
+
+                Else
+                    MessageBox.Show("Failed to connect the database")
+
+                End If
+
+            End If
+
+        Else
+            MessageBox.Show("Please Select a customer to update")
+        End If
+    End Sub
+
+    Private Sub Delete_cust_btn_Click(sender As Object, e As EventArgs) Handles Delete_cust_btn.Click
+        If customer_datagridview.SelectedRows.Count > 0 Then
+            Dim selectedRow As DataGridViewRow = customer_datagridview.SelectedRows(0)
+            Dim Cust_ID As Integer = Convert.ToInt32(selectedRow.Cells("cl_ID").Value)
+            If openDB() Then
+                Dim query As String = "DELETE FROM customer WHERE Cust_ID = @c_id"
+                Dim cmd As New MySqlCommand(query, Conn)
+                cmd.Parameters.AddWithValue("@c_id", Cust_ID)
+
+                Try
+                    cmd.ExecuteNonQuery()
+                    customer_datagridview.Rows.Clear()
+                    LoadDataCustomer()
+
+                    txt_cname.Clear()
+                    txt_caddress.Clear()
+                    txt_cemail.Clear()
+                    txt_cnumber.Clear()
+
+
+
+                Catch ex As Exception
+
+                    MessageBox.Show(ex.Message)
+
+                Finally
+                    closeDB()
+
+                End Try
+            Else
+                MessageBox.Show("The database is not connected!")
+
+            End If
+
+        Else
+            MessageBox.Show("Please select customer to delete!")
+
+
+        End If
+
+    End Sub
+
+    Private Sub Clear_cust_btn_Click(sender As Object, e As EventArgs) Handles Clear_cust_btn.Click
+        If customer_datagridview.SelectedRows.Count > 0 Then
+            customer_datagridview.ClearSelection()
+        End If
+
+
+        txt_cname.Clear()
+        txt_caddress.Clear()
+        txt_cemail.Clear()
+        txt_cnumber.Clear()
+
+    End Sub
+
+
+
+
 End Class
