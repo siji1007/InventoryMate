f380fe00d2bb60172c3baf6c8b6e4a9841c49c45
Christian John Ibañez <109792510+siji1007@users.noreply.github.com>
2024-04-17 02:49:09 +0800

    updated code bukas naman

--- EMPLOYEE.Designer.vb
+++ EMPLOYEE.Designer.vb
@@ -22,7 +22,38 @@ Partial Class EMPLOYEE
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Dim DataGridViewCellStyle7 As DataGridViewCellStyle = New DataGridViewCellStyle()
+        Dim DataGridViewCellStyle8 As DataGridViewCellStyle = New DataGridViewCellStyle()
+        Dim DataGridViewCellStyle9 As DataGridViewCellStyle = New DataGridViewCellStyle()
         lbl_employee = New Label()
+        emp_datagridview = New DataGridView()
+        Emp_ID = New DataGridViewTextBoxColumn()
+        Emp_name = New DataGridViewTextBoxColumn()
+        Emp_address = New DataGridViewTextBoxColumn()
+        Emp_cnumber = New DataGridViewTextBoxColumn()
+        Emp_bdate = New DataGridViewTextBoxColumn()
+        Emp_status = New DataGridViewTextBoxColumn()
+        txt_emp_name = New TextBox()
+        txt_emp_cnumber = New TextBox()
+        emp_birthdate = New DateTimePicker()
+        Button1 = New Button()
+        lbl_name_sup = New Label()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        status_menu = New ContextMenuStrip(components)
+        ACTIVE = New ToolStripMenuItem()
+        OFFLINE = New ToolStripMenuItem()
+        Btn_add = New Button()
+        Btn_update = New Button()
+        Btn_delete = New Button()
+        Btn_clear = New Button()
+        txt_emp_address = New TextBox()
+        Label4 = New Label()
+        txt_search = New TextBox()
+        CType(emp_datagridview, ComponentModel.ISupportInitialize).BeginInit()
+        status_menu.SuspendLayout()
         SuspendLayout()
         ' 
         ' lbl_employee
@@ -37,19 +68,320 @@ Partial Class EMPLOYEE
         lbl_employee.TabIndex = 0
         lbl_employee.Text = "EMPLOYEE"
         ' 
+        ' emp_datagridview
+        ' 
+        emp_datagridview.Anchor = AnchorStyles.Left Or AnchorStyles.Right
+        emp_datagridview.BackgroundColor = Color.FromArgb(CByte(30), CByte(39), CByte(46))
+        DataGridViewCellStyle7.Alignment = DataGridViewContentAlignment.MiddleCenter
+        DataGridViewCellStyle7.BackColor = Color.FromArgb(CByte(30), CByte(39), CByte(46))
+        DataGridViewCellStyle7.Font = New Font("Microsoft Sans Serif", 14.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        DataGridViewCellStyle7.ForeColor = SystemColors.Window
+        DataGridViewCellStyle7.SelectionBackColor = SystemColors.Highlight
+        DataGridViewCellStyle7.SelectionForeColor = SystemColors.HighlightText
+        DataGridViewCellStyle7.WrapMode = DataGridViewTriState.True
+        emp_datagridview.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle7
+        emp_datagridview.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        emp_datagridview.Columns.AddRange(New DataGridViewColumn() {Emp_ID, Emp_name, Emp_address, Emp_cnumber, Emp_bdate, Emp_status})
+        DataGridViewCellStyle8.Alignment = DataGridViewContentAlignment.MiddleCenter
+        DataGridViewCellStyle8.BackColor = Color.FromArgb(CByte(30), CByte(39), CByte(46))
+        DataGridViewCellStyle8.Font = New Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        DataGridViewCellStyle8.ForeColor = Color.White
+        DataGridViewCellStyle8.SelectionBackColor = SystemColors.Highlight
+        DataGridViewCellStyle8.SelectionForeColor = SystemColors.HighlightText
+        DataGridViewCellStyle8.WrapMode = DataGridViewTriState.False
+        emp_datagridview.DefaultCellStyle = DataGridViewCellStyle8
+        emp_datagridview.GridColor = SystemColors.Window
+        emp_datagridview.Location = New Point(12, 85)
+        emp_datagridview.Name = "emp_datagridview"
+        emp_datagridview.ReadOnly = True
+        DataGridViewCellStyle9.Alignment = DataGridViewContentAlignment.MiddleCenter
+        DataGridViewCellStyle9.BackColor = Color.FromArgb(CByte(30), CByte(39), CByte(46))
+        DataGridViewCellStyle9.Font = New Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        DataGridViewCellStyle9.ForeColor = Color.White
+        DataGridViewCellStyle9.SelectionBackColor = SystemColors.Highlight
+        DataGridViewCellStyle9.SelectionForeColor = SystemColors.HighlightText
+        DataGridViewCellStyle9.WrapMode = DataGridViewTriState.True
+        emp_datagridview.RowHeadersDefaultCellStyle = DataGridViewCellStyle9
+        emp_datagridview.RowHeadersVisible = False
+        emp_datagridview.Size = New Size(1109, 301)
+        emp_datagridview.TabIndex = 1
+        ' 
+        ' Emp_ID
+        ' 
+        Emp_ID.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells
+        Emp_ID.HeaderText = "ID"
+        Emp_ID.Name = "Emp_ID"
+        Emp_ID.ReadOnly = True
+        Emp_ID.Width = 52
+        ' 
+        ' Emp_name
+        ' 
+        Emp_name.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill
+        Emp_name.HeaderText = "NAME"
+        Emp_name.Name = "Emp_name"
+        Emp_name.ReadOnly = True
+        ' 
+        ' Emp_address
+        ' 
+        Emp_address.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill
+        Emp_address.HeaderText = "ADDRESS"
+        Emp_address.Name = "Emp_address"
+        Emp_address.ReadOnly = True
+        ' 
+        ' Emp_cnumber
+        ' 
+        Emp_cnumber.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill
+        Emp_cnumber.HeaderText = "CONTACT NUMBER"
+        Emp_cnumber.Name = "Emp_cnumber"
+        Emp_cnumber.ReadOnly = True
+        ' 
+        ' Emp_bdate
+        ' 
+        Emp_bdate.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill
+        Emp_bdate.HeaderText = "BIRTHDATE"
+        Emp_bdate.Name = "Emp_bdate"
+        Emp_bdate.ReadOnly = True
+        ' 
+        ' Emp_status
+        ' 
+        Emp_status.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells
+        Emp_status.HeaderText = "ID STATUS"
+        Emp_status.Name = "Emp_status"
+        Emp_status.ReadOnly = True
+        Emp_status.Width = 120
+        ' 
+        ' txt_emp_name
+        ' 
+        txt_emp_name.Location = New Point(143, 450)
+        txt_emp_name.Name = "txt_emp_name"
+        txt_emp_name.PlaceholderText = " ex. Christan John Ibanez"
+        txt_emp_name.Size = New Size(230, 23)
+        txt_emp_name.TabIndex = 2
+        ' 
+        ' txt_emp_cnumber
+        ' 
+        txt_emp_cnumber.Location = New Point(143, 535)
+        txt_emp_cnumber.Name = "txt_emp_cnumber"
+        txt_emp_cnumber.Size = New Size(230, 23)
+        txt_emp_cnumber.TabIndex = 3
+        txt_emp_cnumber.Text = "+63"
+        ' 
+        ' emp_birthdate
+        ' 
+        emp_birthdate.Anchor = AnchorStyles.Top Or AnchorStyles.Right
+        emp_birthdate.Location = New Point(770, 450)
+        emp_birthdate.Name = "emp_birthdate"
+        emp_birthdate.ShowCheckBox = True
+        emp_birthdate.Size = New Size(230, 23)
+        emp_birthdate.TabIndex = 4
+        ' 
+        ' Button1
+        ' 
+        Button1.Anchor = AnchorStyles.Top Or AnchorStyles.Right
+        Button1.Location = New Point(770, 482)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(102, 31)
+        Button1.TabIndex = 6
+        Button1.Text = ">>"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' lbl_name_sup
+        ' 
+        lbl_name_sup.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left
+        lbl_name_sup.AutoSize = True
+        lbl_name_sup.BackColor = Color.Transparent
+        lbl_name_sup.Font = New Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        lbl_name_sup.ForeColor = Color.White
+        lbl_name_sup.Location = New Point(22, 454)
+        lbl_name_sup.Name = "lbl_name_sup"
+        lbl_name_sup.Size = New Size(55, 20)
+        lbl_name_sup.TabIndex = 8
+        lbl_name_sup.Text = "NAME"
+        ' 
+        ' Label1
+        ' 
+        Label1.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left
+        Label1.AutoSize = True
+        Label1.BackColor = Color.Transparent
+        Label1.Font = New Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label1.ForeColor = Color.White
+        Label1.Location = New Point(22, 537)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(83, 20)
+        Label1.TabIndex = 9
+        Label1.Text = "CONTACT"
+        ' 
+        ' Label2
+        ' 
+        Label2.Anchor = AnchorStyles.Top Or AnchorStyles.Right
+        Label2.AutoSize = True
+        Label2.BackColor = Color.Transparent
+        Label2.Font = New Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label2.ForeColor = Color.Black
+        Label2.Location = New Point(649, 453)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(101, 20)
+        Label2.TabIndex = 10
+        Label2.Text = "BIRTHDATE"
+        ' 
+        ' Label3
+        ' 
+        Label3.Anchor = AnchorStyles.Top Or AnchorStyles.Right
+        Label3.AutoSize = True
+        Label3.BackColor = Color.Transparent
+        Label3.Font = New Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label3.ForeColor = Color.Black
+        Label3.Location = New Point(648, 493)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(93, 20)
+        Label3.TabIndex = 11
+        Label3.Text = "ID STATUS"
+        ' 
+        ' status_menu
+        ' 
+        status_menu.Items.AddRange(New ToolStripItem() {ACTIVE, OFFLINE})
+        status_menu.Name = "status_menu"
+        status_menu.Size = New Size(120, 48)
+        ' 
+        ' ACTIVE
+        ' 
+        ACTIVE.Name = "ACTIVE"
+        ACTIVE.Size = New Size(119, 22)
+        ACTIVE.Text = "ACTIVE"
+        ' 
+        ' OFFLINE
+        ' 
+        OFFLINE.Name = "OFFLINE"
+        OFFLINE.Size = New Size(119, 22)
+        OFFLINE.Text = "OFFLINE"
+        ' 
+        ' Btn_add
+        ' 
+        Btn_add.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left
+        Btn_add.Location = New Point(27, 814)
+        Btn_add.Name = "Btn_add"
+        Btn_add.Size = New Size(111, 53)
+        Btn_add.TabIndex = 12
+        Btn_add.Text = "CREATE"
+        Btn_add.UseVisualStyleBackColor = True
+        ' 
+        ' Btn_update
+        ' 
+        Btn_update.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left
+        Btn_update.Location = New Point(266, 814)
+        Btn_update.Name = "Btn_update"
+        Btn_update.Size = New Size(111, 53)
+        Btn_update.TabIndex = 13
+        Btn_update.Text = "UPDATE"
+        Btn_update.UseVisualStyleBackColor = True
+        ' 
+        ' Btn_delete
+        ' 
+        Btn_delete.Anchor = AnchorStyles.Bottom Or AnchorStyles.Right
+        Btn_delete.Location = New Point(789, 814)
+        Btn_delete.Name = "Btn_delete"
+        Btn_delete.Size = New Size(111, 53)
+        Btn_delete.TabIndex = 14
+        Btn_delete.Text = "DELETE"
+        Btn_delete.UseVisualStyleBackColor = True
+        ' 
+        ' Btn_clear
+        ' 
+        Btn_clear.Anchor = AnchorStyles.Bottom Or AnchorStyles.Right
+        Btn_clear.Location = New Point(1012, 814)
+        Btn_clear.Name = "Btn_clear"
+        Btn_clear.Size = New Size(111, 53)
+        Btn_clear.TabIndex = 15
+        Btn_clear.Text = "CLEAR"
+        Btn_clear.UseVisualStyleBackColor = True
+        ' 
+        ' txt_emp_address
+        ' 
+        txt_emp_address.Location = New Point(143, 490)
+        txt_emp_address.Name = "txt_emp_address"
+        txt_emp_address.PlaceholderText = " ex. Talisay, Camarines Norte"
+        txt_emp_address.Size = New Size(230, 23)
+        txt_emp_address.TabIndex = 16
+        ' 
+        ' Label4
+        ' 
+        Label4.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left
+        Label4.AutoSize = True
+        Label4.BackColor = Color.Transparent
+        Label4.Font = New Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label4.ForeColor = Color.White
+        Label4.Location = New Point(22, 493)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(89, 20)
+        Label4.TabIndex = 17
+        Label4.Text = "ADDRESS"
+        ' 
+        ' txt_search
+        ' 
+        txt_search.Anchor = AnchorStyles.Top Or AnchorStyles.Right
+        txt_search.Location = New Point(908, 56)
+        txt_search.Name = "txt_search"
+        txt_search.PlaceholderText = "Search"
+        txt_search.Size = New Size(213, 23)
+        txt_search.TabIndex = 18
+        ' 
         ' EMPLOYEE
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         BackgroundImage = My.Resources.Resources.m0sag04
         BackgroundImageLayout = ImageLayout.Center
-        ClientSize = New Size(800, 450)
+        ClientSize = New Size(1133, 890)
         ControlBox = False
+        Controls.Add(txt_search)
+        Controls.Add(Label4)
+        Controls.Add(txt_emp_address)
+        Controls.Add(Btn_clear)
+        Controls.Add(Btn_delete)
+        Controls.Add(Btn_update)
+        Controls.Add(Btn_add)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(lbl_name_sup)
+        Controls.Add(Button1)
+        Controls.Add(emp_birthdate)
+        Controls.Add(txt_emp_cnumber)
+        Controls.Add(txt_emp_name)
+        Controls.Add(emp_datagridview)
         Controls.Add(lbl_employee)
         Name = "EMPLOYEE"
+        WindowState = FormWindowState.Maximized
+        CType(emp_datagridview, ComponentModel.ISupportInitialize).EndInit()
+        status_menu.ResumeLayout(False)
         ResumeLayout(False)
         PerformLayout()
     End Sub
 
     Friend WithEvents lbl_employee As Label
+    Friend WithEvents emp_datagridview As DataGridView
+    Friend WithEvents txt_emp_name As TextBox
+    Friend WithEvents txt_emp_cnumber As TextBox
+    Friend WithEvents emp_birthdate As DateTimePicker
+    Friend WithEvents Button1 As Button
+    Friend WithEvents lbl_name_sup As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents status_menu As ContextMenuStrip
+    Friend WithEvents ACTIVE As ToolStripMenuItem
+    Friend WithEvents OFFLINE As ToolStripMenuItem
+    Friend WithEvents Btn_add As Button
+    Friend WithEvents Btn_update As Button
+    Friend WithEvents Btn_delete As Button
+    Friend WithEvents Btn_clear As Button
+    Friend WithEvents Emp_ID As DataGridViewTextBoxColumn
+    Friend WithEvents Emp_name As DataGridViewTextBoxColumn
+    Friend WithEvents Emp_address As DataGridViewTextBoxColumn
+    Friend WithEvents Emp_cnumber As DataGridViewTextBoxColumn
+    Friend WithEvents Emp_bdate As DataGridViewTextBoxColumn
+    Friend WithEvents Emp_status As DataGridViewTextBoxColumn
+    Friend WithEvents txt_emp_address As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txt_search As TextBox
 End Class
--- EMPLOYEE.vb
+++ EMPLOYEE.vb
@@ -1,4 +1,11 @@
-﻿Public Class EMPLOYEE
+﻿Imports System.Net.NetworkInformation
+Imports System.Text.RegularExpressions
+Imports MySql.Data.MySqlClient
+
+Public Class EMPLOYEE
+
+    Private selectedStatus As String = ""
+
     Private Sub lbl_employee_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         lbl_employee.Anchor = AnchorStyles.None
         lbl_employee.TextAlign = ContentAlignment.MiddleCenter
@@ -8,6 +15,225 @@
 
         Dim centerX As Integer = (Me.ClientSize.Width - lbl_employee.Width) \ 2
         lbl_employee.Location = New Point(centerX, lbl_employee.Location.Y)
+        DataLoadEmployee()
+
+    End Sub
+
+
+    Private Sub DataLoadEmployee()
+        Try
+            If openDB() Then
+                Dim query As New MySqlCommand("SELECT Emp_ID, Emp_name, Emp_address, Emp_cnumber, DATE_FORMAT(Emp_bdate, '%b-%e-%Y') AS Emp_bdate, Emp_status FROM employee", Conn)
+                Using dr As MySqlDataReader = query.ExecuteReader
+                    While dr.Read
+                        ' Concatenate "+63" to the beginning of Emp_cnumber
+                        Dim formattedCnumber As String = "+63" & dr.Item("Emp_cnumber").ToString()
+                        Dim formattedDate As String = dr.Item("Emp_bdate").ToString() ' Convert date to string
+                        Dim rowIndex As Integer = emp_datagridview.Rows.Add(dr.Item("Emp_ID"), dr.Item("Emp_name"), dr.Item("Emp_address"), formattedCnumber, formattedDate, dr.Item("Emp_status"))
+
+
+                        Dim Status As String = dr.Item("Emp_status")
+                        If Status = "OFFLINE" Then
+                            emp_datagridview.Rows(rowIndex).DefaultCellStyle.BackColor = Color.Red
+                        End If
+
+                    End While
+                End Using
+            Else
+                MessageBox.Show("Failed to connect to database")
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        status_menu.Show(Button1, 0, Button1.Height)
+    End Sub
+
+    Private Sub MenuItem_Click(sender As Object, e As EventArgs) Handles ACTIVE.Click, OFFLINE.Click
+        Dim menuItem As ToolStripMenuItem = CType(sender, ToolStripMenuItem)
+        Button1.Text = menuItem.Text
+        selectedStatus = menuItem.Text
+    End Sub
+
+
+    'THIS IS FOR ADD BUTTON 
+    Private Sub Btn_add_Click(sender As Object, e As EventArgs) Handles Btn_add.Click
+        Dim selectedDate As Date = emp_birthdate.Value
+        ' Format the date correctly for MySQL
+        Dim formattedDate As String = selectedDate.ToString("yyyy-MM-dd")
+
+        'Trim the textbox 
+        Dim Emp_name As String = txt_emp_name.Text.Trim()
+        Dim Emp_address As String = txt_emp_address.Text.Trim()
+
+        ' Remove "+63" prefix from phone number if present
+        Dim Emp_cnumber As String = txt_emp_cnumber.Text.Trim()
+        If Emp_cnumber.StartsWith("+63") Then
+            Emp_cnumber = Emp_cnumber.Substring(3) ' Remove "+63" prefix
+        End If
+
+        ' Ensure Emp_cnumber contains exactly 10 digits
+        If Emp_cnumber.Length <> 10 OrElse Not Emp_cnumber.All(AddressOf Char.IsDigit) Then
+            MessageBox.Show("Please enter a valid 10-digit phone number.")
+        Else
+            If openDB() Then
+                Dim query As String = "INSERT INTO employee (Emp_name, Emp_address, Emp_cnumber, Emp_bdate, Emp_status) VALUES (@Emp_name, @Emp_address, @Emp_cnumber, STR_TO_DATE(@Emp_bdate, '%Y-%m-%d'), @Emp_status)"
+                Dim cmd As New MySqlCommand(query, Conn)
+                cmd.Parameters.AddWithValue("@Emp_name", Emp_name.ToUpper())
+                cmd.Parameters.AddWithValue("@Emp_address", Emp_address.ToUpper())
+                cmd.Parameters.AddWithValue("@Emp_cnumber", Emp_cnumber)
+                cmd.Parameters.AddWithValue("@Emp_bdate", formattedDate) ' Use formatted date
+                cmd.Parameters.AddWithValue("@Emp_status", selectedStatus.ToUpper())
+
+                Try
+                    cmd.ExecuteNonQuery()
+                    MessageBox.Show("Successfully added")
+                    emp_datagridview.Rows.Clear()
+                    DataLoadEmployee()
+
+                    txt_emp_name.Clear()
+                    txt_emp_address.Clear()
+                    txt_emp_cnumber.Clear()
+                    emp_birthdate.Value = DateTime.Today
+                    Button1.Text = ">>"
+                Catch ex As Exception
+                    MessageBox.Show(ex.Message)
+                End Try
+            Else
+                MessageBox.Show("The database is not connected")
+            End If
+        End If
+    End Sub
+
+
+
+    'DATAGRIDVIEW EVENTS BY CLICKING ONE FOR ALL ROWS 
+    Private Sub emp_datagridview_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles emp_datagridview.CellContentClick
+        If e.RowIndex >= 0 AndAlso e.ColumnIndex >= 0 Then
+            Dim selectedRow As DataGridViewRow = emp_datagridview.Rows(e.RowIndex)
+
+            Dim EmpId As String = selectedRow.Cells("Emp_ID").Value.ToString()
+            Dim EmpName As String = selectedRow.Cells("Emp_name").Value.ToString()
+            Dim EmpAddress As String = selectedRow.Cells("Emp_address").Value.ToString()
+            Dim EmpCnumber As Long = Convert.ToInt64(selectedRow.Cells("Emp_cnumber").Value)
+            Dim EmpBdate As String = selectedRow.Cells("Emp_bdate").Value.ToString()
+            Dim EmpStatus As String = selectedRow.Cells("Emp_status").Value.ToString()
+
+
+
+            For Each row As DataGridViewRow In emp_datagridview.Rows
+                If row.Index = e.RowIndex Then
+                    row.Selected = True
+                Else
+                    row.Selected = False
+                End If
+
+            Next
+
+
+            'Add the selected value in datagrid in their corresponding textbox 
+            txt_emp_name.Text = EmpName
+            txt_emp_address.Text = EmpAddress
+            txt_emp_cnumber.Text = "+" & EmpCnumber
+            emp_birthdate.Value = EmpBdate
+            Button1.Text = EmpStatus
+
+        End If
+    End Sub
+
+    Private Sub Btn_update_Click(sender As Object, e As EventArgs) Handles Btn_update.Click
+        Dim selectedDate As Date = emp_birthdate.Value
+        Dim Emp_ID As String ' Change data type to String for phone numbers
+
+        Emp_ID = emp_datagridview.CurrentRow.Cells("Emp_ID").Value.ToString() ' Assuming Emp_ID is a String
+
+        Dim Emp_name As String = txt_emp_name.Text
+        Dim Emp_address As String = txt_emp_address.Text
+        Dim Emp_cnumber As String = txt_emp_cnumber.Text.Trim() ' Remove leading/trailing whitespace
+
+        ' Remove "+63" prefix if present
+        If Emp_cnumber.StartsWith("+63") Then
+            Emp_cnumber = Emp_cnumber.Substring(3) ' Remove "+63" prefix
+        End If
+
+        Dim Emp_bdate As String = selectedDate.ToString("yyyy-MM-dd")
+        Dim Emp_status As String = selectedStatus
+
+        ' Validate phone number format
+        If Emp_cnumber.Length = 10 AndAlso Emp_cnumber.All(AddressOf Char.IsDigit) Then
+            ' Valid phone number format
+            If openDB() Then
+                Dim query As String = "UPDATE employee SET Emp_name = @Emp_name, Emp_address = @Emp_address, Emp_cnumber = @Emp_cnumber, Emp_bdate = @Emp_bdate, Emp_status = @Emp_status WHERE Emp_ID = @Emp_ID"
+                Dim cmd As New MySqlCommand(query, Conn)
+                cmd.Parameters.AddWithValue("@Emp_ID", Emp_ID)
+                cmd.Parameters.AddWithValue("@Emp_name", Emp_name)
+                cmd.Parameters.AddWithValue("@Emp_address", Emp_address)
+                cmd.Parameters.AddWithValue("@Emp_cnumber", Emp_cnumber)
+                cmd.Parameters.AddWithValue("@Emp_bdate", Emp_bdate)
+                cmd.Parameters.AddWithValue("@Emp_status", Emp_status)
+                Try
+                    cmd.ExecuteNonQuery()
+
+                    emp_datagridview.Rows.Clear()
+                    DataLoadEmployee()
+
+                    txt_emp_name.Clear()
+                    txt_emp_address.Clear()
+                    txt_emp_cnumber.Clear()
+                    emp_birthdate.Value = DateTime.Today
+                    Button1.Text = ">>"
+                    MessageBox.Show("Employee successfully updated!")
+                Catch ex As Exception
+                    MessageBox.Show($"Error: {ex.Message}")
+                Finally
+                    closeDB()
+                End Try
+            Else
+                MessageBox.Show("The database failed to connect!")
+            End If
+        Else
+            MessageBox.Show("Please enter a valid 10-digit phone number.")
+        End If
+    End Sub
+
+    Private Sub txt_search_TextChanged(sender As Object, e As EventArgs) Handles txt_search.TextChanged
+        Dim searchValue As String = txt_search.Text.Trim().ToLower()
+
+        For Each row As DataGridViewRow In emp_datagridview.Rows
+            If Not row.IsNewRow Then
+                Dim EmpName As String = row.Cells("Emp_name").Value.ToString().ToLower()
+                If searchValue = "" OrElse EmpName.Contains(searchValue) Then
+                    row.Visible = True
+                Else
+                    row.Visible = False
+                End If
+            End If
+        Next
+
+
 
     End Sub
 End Class
